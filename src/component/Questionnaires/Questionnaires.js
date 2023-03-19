3a441205c3e2550f4bbdeeeb20fe77840b51e341
krima <krima@LAPTOP-VTDEO2HJ.localdomain>
2023-03-19 13:54:36 +0300

    add footer to Questionnaires

--- src/component/Questionnaires/Questionnaires.js
+++ src/component/Questionnaires/Questionnaires.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
 
 import QuestionnairesCard from './QuestionnairesCard/QuestionnairesCard';
+import Footer from '../Footer/Footer';
 
 import classes from './Questionnaires.module.scss'
 
-const Questionnaires = ({ id, go, data }) => {
+const Questionnaires = ({ id, go, data, activePanel }) => {
     return (
         <Panel id={id}>
             {/* <PanelHeader before={<PanelHeaderBack onClick={go} data-to="home"/>}>
